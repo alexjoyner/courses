f856e309db91b6a6c13173eda263f8f5d4f2b883
alexjoyner <rosco9awj@gmail.com>
2019-11-03 10:35:47 -0500

    Finished 04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,92 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// numOfAddresses = sizeOfScreen
+@8191
+D=A
+@numOfAddresses
+M=D
+@currentValue
+M=0
+@lastValue
+M=0
+// fillValue = 0
+@fillValue
+M=-1
+
+(LISTENING)
+// While true
+  // currentValue = KBD
+  @KBD
+  D=M
+  @currentValue
+  M=D
+  // if lastvalue - currentvalue == 0, don't fill screen
+  @lastValue
+  D=M-D
+  @LISTENING
+  D;JEQ
+
+  // lastValue = currentValue
+  @currentValue
+  D=M
+  @lastValue
+  M=D
+
+  // If D == 0
+  //   Jump to white screen
+  @fillValue
+  M=0
+  @FILL_SCREEN
+  D;JEQ
+
+
+  // Else
+  //   Jump to Black Screen
+  @fillValue
+  M=-1
+  @FILL_SCREEN
+  D;JNE
+
+
+
+(FILL_SCREEN)
+// i = 0
+@i
+M=0
+// Fill Address
+@nextAddress
+M=0
+// do
+(FILL_NEXT)
+// address = i + screen address
+@SCREEN
+D=A
+@i
+D=D+M
+@nextAddress
+M=D
+// SCREEN[ADDRESS] = fillValue
+@fillValue
+D=M
+@nextAddress
+A=M
+M=D
+// While numOfAddresses - i = 0
+@i
+D=M
+@numOfAddresses
+D=M-D
+// i++
+@i
+M=M+1
+// Jmp to (FILL_SCREEN)
+@FILL_NEXT
+D;JGT
+// Jmp to (LISTENING)
+@LISTENING
+0;JMP
+
+(END)
+@END
+0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,47 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// R0
+// R1
+
+// R2 = 0;
+@R2
+M=0
+
+// sum = 0;
+@sum
+M=0
+
+// i = 0;
+@i
+M=0
+
+// do
+(ADD_NUMS)
+// sum = sum + R1
+@R1
+D=M
+@sum
+M=D+M
+
+// i++
+@i
+M=M+1
+
+// while i < R0
+@i
+D=M
+@R0
+D=M-D
+@ADD_NUMS
+D;JNE
+
+// R2 = sum
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
