9e66bc80a999b1d7780e1e3dfc80154b944a4231
Alex Joyner <alex@voicir.com>
2020-03-25 00:09:23 -0400

    relational data basics

--- section-2/graphql-basics/src/index.js
+++ section-2/graphql-basics/src/index.js
@@ -1,8 +1,56 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Demo User Data
+const users = [
+	{
+		id: '1',
+		name: 'Andrew',
+		email: 'andrew@example.com',
+		age: 27
+	},
+	{
+		id: '2',
+		name: 'Sarah',
+		email: 'sarah@example.com',
+		age: 15
+	},
+	{
+		id: '3',
+		name: 'Alex',
+		email: 'alex@example.com'
+	}
+];
+
+// Posts Demo Data
+const posts = [
+	{
+		id: '1',
+		title: 'My Post',
+		body: 'This is my first post from GraphQL',
+		published: false,
+		author: '3'
+	},
+	{
+		id: '2',
+		title: 'Test post 2',
+		body: 'aawfelkj ;lakjkaj we;flj owe foij sdfoj a;slkd',
+		published: false,
+		author: '2'
+	},
+	{
+		id: '3',
+		title: 'Blah Blah Blah',
+		body: 'aksj;dflkja;sdfk squirrel dslkfh;lksdjf;lkjs;fe SQUIRRREL',
+		published: false,
+		author: '2'
+	}
+];
+
 // Type Definitions (schema)
 const typeDefs = `
   type Query {
+    posts(query: String): [Post!]!
+    users(query: String): [User!]!
     add(nums: [Float!]!): Float!
     greeting(name: String!, position: String): String!
     grades: [Int!]!
@@ -22,12 +70,31 @@ const typeDefs = `
     title: String!
     body: String!
     published: Boolean!
+    author: User!
   }
 `;
 
 // Resolvers
 const resolvers = {
 	Query: {
+		posts(parent, args) {
+			if (!args.query) {
+				return posts;
+			}
+			return posts.filter(
+				post =>
+					post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+					post.body.toLowerCase().includes(args.query.toLowerCase())
+			);
+		},
+		users(parent, args) {
+			if (!args.query) {
+				return users;
+			}
+			return users.filter(user =>
+				user.name.toLowerCase().includes(args.query.toLowerCase())
+			);
+		},
 		grades() {
 			return [100, 100, 59];
 		},
@@ -51,12 +118,12 @@ const resolvers = {
 			};
 		},
 		post() {
-			return {
-				id: '3',
-				title: 'Test Post',
-				body: 'This is my first post from GraphQL',
-				published: false
-			};
+			return posts[0];
+		}
+	},
+	Post: {
+		author(parent, args) {
+			return users.find(user => user.id === parent.id);
 		}
 	}
 };
