fe30772cda0951f73acb7fe32838b3911f84503c
Alex Joyner <alex@voicir.com>
2020-05-17 20:06:02 -0400

    added implementation test

---
+++ design-patterns-practice/src/creational/factory/implementation.test.ts
@@ -0,0 +1,21 @@
+import { KnifeShop } from './knifeShop';
+
+describe('Knife Factory Implementation', () => {
+	// As is mentioned in the docs, in the factory pattern, the client/programmer does not
+	//   work directly with the Factory.  The factory is a way to just abstract creating objects
+	//   from the shop.  The client still just works directly with the shop.  As is the
+	//   case most of the time in the real world.
+	it('should allow ordering knives', () => {
+		const knifeShop = new KnifeShop();
+		const steakKnife = knifeShop.orderKnife('steak');
+		const status = steakKnife.getState();
+		const result = `The steak knife is ${
+			status.packaged ? 'packaged' : 'not packaged'
+		}, ${status.polished ? 'polished' : 'not polished'},and ${
+			status.sharpened ? 'sharpened' : 'not sharpened'
+		}!`;
+		expect(result).toEqual(
+			`The steak knife is packaged, polished,and sharpened!`
+		);
+	});
+});
